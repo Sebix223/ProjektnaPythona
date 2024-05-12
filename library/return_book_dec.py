b5f6df9175dd32908b3c12c3d28a1b3e3a53879a
sebaros2333 <sebaros99@o2.pl>
2024-05-12 23:42:39 +0200

    project

--- library/return_book_dec.py
+++ library/return_book_dec.py
@@ -2,16 +2,16 @@ import os
 from datetime import date
 
 def dekorator(func):
-    def wrapper(customer_id, book):
-        if os.path.exists(f"dataset/{customer_id}.txt"):
-            with open(f"dataset/{customer_id}.txt", "r") as file:
-                lines = file.readlines()
-                for i, line in enumerate(lines):
-                    if book in line:
-                        lines[i] = line.replace("False", f"{date.today()}")
-            with open(f"dataset/{customer_id}.txt", "w") as file:
-                file.writelines(lines)
-        else:
-            print("Książka nie została wypożyczona")
-        func(customer_id, book)
+    """
+    Decorator function to handle book returning operations.
+
+    args:
+        func (function): Function to be decorated.
+
+    returns:
+        function: Wrapper function.
+    """
+    def wrapper(customer_id, *args):
+        for book in args:
+            func(customer_id, book)
     return wrapper
